894ded237c220401ae9149a8a3b7baa68a49e0a4
Marc Pignat <marc@pignat.org>
2018-10-24 15:43:56 +0200

    log4j simultaneous stdout and syslog output

---
+++ src/main/java/org/pignat/java/testing/log4j2/Main.java
@@ -0,0 +1,22 @@
+package org.pignat.java.testing.log4j2;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+
+	private static final Logger log = LogManager.getLogger(Main.class);
+
+	public static void main(String args[]) {
+		log.info("Starting application dsfasdfas");
+		System.out.println("Hello World");
+		System.err.println("huho");
+		log.info("Execution completed");
+		log.debug("This is a debug message");
+		log.info("This is an info message");
+		log.warn("This is a warn message");
+		log.error("This is an error message");
+		log.fatal("This is a fatal message");
+	}
+
+}
